0b6423eb45ac3ca18cdc2350563014e0acd740ae
Pedro Videgain Barranco <pedrovb04@protonmail.com>
2023-03-09 12:39:46 +0100

    Added nvim remap to fuzzy-find in the current buffer

--- .config/nvim/lua/config_plugins/telescope.lua
+++ .config/nvim/lua/config_plugins/telescope.lua
@@ -2,6 +2,9 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 
+vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find,
+    { sorting_strategy = ascending, prompt_position = top })
+
 -- Use to find files that contain a string. Requires ripgrep.
 vim.keymap.set("n", "<leader>ps",
     function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
