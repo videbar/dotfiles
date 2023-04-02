479dd22e4cd9dcd315262bd8b80f163c183f1f52
Pedro Videgain Barranco <pedro@vide.bar>
2023-04-02 18:37:23 +0200

    Added the nvim plugin treesitter context

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -17,6 +17,9 @@ return require("packer").startup(function(use)
     -- Treesitter
     use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
 
+    -- Treesitter context
+    use "nvim-treesitter/nvim-treesitter-context"
+
     -- Undotree
     use "mbbill/undotree"
 
@@ -29,19 +32,19 @@ return require("packer").startup(function(use)
         branch = "v1.x",
         requires = {
             -- LSP Support
-            { "neovim/nvim-lspconfig" }, -- Required
-            { "williamboman/mason.nvim" }, -- Optional
+            { "neovim/nvim-lspconfig" },           -- Required
+            { "williamboman/mason.nvim" },         -- Optional
             { "williamboman/mason-lspconfig.nvim" }, -- Optional
             -- Autocompletion
-            { "hrsh7th/nvim-cmp" }, -- Required
-            { "hrsh7th/cmp-nvim-lsp" }, -- Required
-            { "hrsh7th/cmp-buffer" }, -- Optional
-            { "hrsh7th/cmp-path" }, -- Optional
-            { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" }, -- Optional
+            { "hrsh7th/nvim-cmp" },                -- Required
+            { "hrsh7th/cmp-nvim-lsp" },            -- Required
+            { "hrsh7th/cmp-buffer" },              -- Optional
+            { "hrsh7th/cmp-path" },                -- Optional
+            { "saadparwaiz1/cmp_luasnip" },        -- Optional
+            { "hrsh7th/cmp-nvim-lua" },            -- Optional
             -- Snippets
-            { "L3MON4D3/LuaSnip" }, -- Required
-            { "rafamadriz/friendly-snippets" }, -- Optional
+            { "L3MON4D3/LuaSnip" },                -- Required
+            { "rafamadriz/friendly-snippets" },    -- Optional
             -- Formatting
             { "jose-elias-alvarez/null-ls.nvim" }
         }
