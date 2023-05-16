6a548e309a9b37ebfcaa37e73c99e566fd977578
Pedro Videgain Barranco <pedrovb04@protonmail.com>
2023-05-16 14:57:22 +0200

    Configured cmp to work with autopairs on nvim

--- .config/nvim/lua/config_plugins/lsp.lua
+++ .config/nvim/lua/config_plugins/lsp.lua
@@ -20,6 +20,10 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ["<S-Tab>"] = nil
 })
 
+-- Include parenthesis and similar delimiters when using autocomplete.
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
 lsp.setup_nvim_cmp({ mapping = cmp_mappings })
 
 -- Disable autocomplete in comments using cmp.
