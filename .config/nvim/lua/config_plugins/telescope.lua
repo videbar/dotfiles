11296127292f14171c8d2f2f2e3e6876972df755
Pedro Videgain Barranco <pedro@vide.bar>
2023-10-11 13:30:06 +0200

    Added spelling-related remaps to the nvim config

--- .config/nvim/lua/config_plugins/telescope.lua
+++ .config/nvim/lua/config_plugins/telescope.lua
@@ -11,3 +11,22 @@ vim.keymap.set("n", "<leader>ps", function()
         if input ~= nil then builtin.grep_string({ search = input }) end
     end)
 end)
+
+-- Show spell check suggestions using telescope with <leader>sp (spell fix).
+vim.keymap.set("n", "<leader>sf", function()
+    require("telescope.builtin").spell_suggest(
+        require("telescope.themes").get_cursor({}))
+end, { desc = "Spelling Suggestions" })
+
+-- Setup remaps to change the language of the spellchecker.
+vim.keymap.set("n", "<leader>sg", function()
+    vim.o.spelllang = "en"
+    vim.opt.spell = true
+    print("Spell check set to English")
+end)
+
+vim.keymap.set("n", "<leader>se", function()
+    vim.o.spelllang = "es"
+    vim.opt.spell = true
+    print("Spell check set to Spanish")
+end)
