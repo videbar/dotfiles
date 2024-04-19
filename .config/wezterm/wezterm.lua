87cfdb5aed76ad9a30f3a2de71417718ecb818cf
Pedro Videgain Barranco <pedro@vide.bar>
2024-04-19 12:59:54 +0200

    Updated my wezterm config
    - Changed the default workspace names to dates and times
    - Added keymaps to easily navigate workspaces
    - Updated window title to show the current workspace
    - Added a specific keymap to split panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,9 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = wezterm.config_builder()
+local function generate_new_workspace_name()
+    return "Created on " .. os.date("%Y/%m/%d - %H:%M:%S")
+end
 
 config.enable_tab_bar = false
 config.tab_bar_at_bottom = true
@@ -11,7 +14,7 @@ config.font_size = 11.0
 config.command_palette_fg_color = "#7dcfff"
 config.command_palette_bg_color = "#1f2335"
 config.color_scheme = "tokyonight_storm"
-config.default_workspace = "main"
+config.default_workspace = generate_new_workspace_name()
 config.enable_wayland = false
 config.disable_default_key_bindings = true
 
@@ -20,21 +23,8 @@ local function workspace_navigator()
     return wezterm.action_callback(function(window, pane)
         local workspaces = wezterm.mux.get_workspace_names()
         local choices = {}
-        -- Keeps track of whether or not the default workspace is opened. Used so that
-        -- the default always appears at the top of the navigator.
-        local has_default = false
         for _, ws in ipairs(workspaces) do
-            if ws == config.default_workspace then
-                has_default = true
-            else
-                table.insert(choices, { label = ws, id = ws })
-            end
-        end
-        if has_default then
-            table.insert(choices, 1, {
-                label = config.default_workspace,
-                id = config.default_workspace,
-            })
+            table.insert(choices, { label = ws, id = ws })
         end
         table.insert(choices, {
             label = wezterm.format({
@@ -48,11 +38,16 @@ local function workspace_navigator()
                 title = "Workspaces",
                 choices = choices,
                 action = wezterm.action_callback(
-                    function(_sub_window, _sub_pane, id, _label)
+                    function(sub_window, sub_pane, id, label)
                         if id == nil then
                             return
                         elseif id == "new" then
-                            window:perform_action(act.SwitchToWorkspace, pane)
+                            window:perform_action(
+                                act.SwitchToWorkspace({
+                                    name = generate_new_workspace_name(),
+                                }),
+                                pane
+                            )
                         else
                             wezterm.mux.set_active_workspace(id)
                         end
@@ -64,63 +59,112 @@ local function workspace_navigator()
     end)
 end
 
-local function try_move_or_split(direction)
-    -- Try to move to a pane situated in a given direction. If there is no pane, create
-    -- a new one.
+local function set_active_workspace_by_number(n)
     return wezterm.action_callback(function(window, pane)
-        local current_tab = window:active_tab()
-        if current_tab:get_pane_direction(direction) == nil then
-            wezterm.log_info(direction .. " is nil")
-            window:perform_action(act.SplitPane({ direction = direction }), pane)
-        else
-            wezterm.log_info("Moving " .. direction)
-            window:perform_action(act.ActivatePaneDirection(direction), pane)
+        local workspaces = wezterm.mux.get_workspace_names()
+        if n > #workspaces then
+            wezterm.log_info("The value of n is too high")
+            return
+        end
+        wezterm.log_info("Workspaces:\n", workspaces)
+        wezterm.mux.set_active_workspace(workspaces[n])
+    end)
+end
+
+local function try_split_or_unzoom(direction)
+    -- If the current pane is zoomed, unzoom it, otherwise split it.
+    return wezterm.action_callback(function(window, pane)
+        local id = pane:pane_id()
+        local panes_info = window:active_tab():panes_with_info()
+        for _, info in ipairs(panes_info) do
+            if info.pane:pane_id() == id then
+                if info.is_zoomed then
+                    wezterm.log_info("Pane is zoomed")
+                    window:perform_action(act.SetPaneZoomState(false), pane)
+                else
+                    wezterm.log_info("Pane is not zoomed, splitting " .. direction)
+                    pane:split({ direction = direction })
+                end
+                return
+            end
         end
     end)
 end
+
+wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+    local current_workspace = wezterm.mux.get_active_workspace()
+    local workspace_idx = 0
+    local all_workspaces = wezterm.mux.get_workspace_names()
+    for i, ws in ipairs(all_workspaces) do
+        if ws == current_workspace then
+            workspace_idx = i
+            break
+        end
+    end
+    local zoomed = ""
+
+    if tab.active_pane.is_zoomed then
+        zoomed = "[Z] "
+    end
+    local workspace_str = string.format("[%d/%d] ", workspace_idx, #all_workspaces)
+
+    return zoomed .. workspace_str .. tab.active_pane.title
+end)
 
 -- Keys
 config.leader = { key = "phys:Space", mods = "CTRL", timeout_milliseconds = 1000 }
 
 config.keys = {
-    { key = "c", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
-
-    -- Paste from the clipboard
+    { key = "c", mods = "LEADER", action = act.ActivateCopyMode },
     { key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 
     { key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
     -- Panes
-    {
-        key = "s",
-        mods = "LEADER",
-        action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-    },
-    {
-        key = "v",
-        mods = "LEADER",
-        action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    },
-    { key = "h", mods = "LEADER", action = try_move_or_split("Left") },
-    { key = "j", mods = "LEADER", action = try_move_or_split("Down") },
-    { key = "k", mods = "LEADER", action = try_move_or_split("Up") },
-    { key = "l", mods = "LEADER", action = try_move_or_split("Right") },
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+
+    { key = "v", mods = "LEADER", action = try_split_or_unzoom("Right") },
+    { key = "s", mods = "LEADER", action = try_split_or_unzoom("Bottom") },
+
     { key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
     { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
     { key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
-    -- Tabs
     {
         key = "r",
         mods = "LEADER",
-        action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+        action = act.ActivateKeyTable({
+            name = "resize_pane",
+            one_shot = false,
+            until_unknown = true,
+        }),
     },
-    { key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-    { key = "u", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-    { key = "i", mods = "LEADER", action = act.ActivateTabRelative(1) },
-    { key = "e", mods = "LEADER", action = act.ShowTabNavigator },
     -- Workspaces
     { key = "w", mods = "LEADER", action = workspace_navigator() },
     { key = "n", mods = "LEADER", action = act.SwitchWorkspaceRelative(1) },
     { key = "p", mods = "LEADER", action = act.SwitchWorkspaceRelative(-1) },
+    {
+        mods = "LEADER",
+        key = "e",
+        action = wezterm.action_callback(function(window, pane)
+            window:perform_action(
+                act.SwitchToWorkspace({
+                    name = generate_new_workspace_name(),
+                }),
+                pane
+            )
+        end),
+    },
+    { key = "1", mods = "LEADER", action = set_active_workspace_by_number(1) },
+    { key = "2", mods = "LEADER", action = set_active_workspace_by_number(2) },
+    { key = "3", mods = "LEADER", action = set_active_workspace_by_number(3) },
+    { key = "4", mods = "LEADER", action = set_active_workspace_by_number(4) },
+    { key = "5", mods = "LEADER", action = set_active_workspace_by_number(5) },
+    { key = "6", mods = "LEADER", action = set_active_workspace_by_number(6) },
+    { key = "7", mods = "LEADER", action = set_active_workspace_by_number(7) },
+    { key = "8", mods = "LEADER", action = set_active_workspace_by_number(8) },
+    { key = "9", mods = "LEADER", action = set_active_workspace_by_number(9) },
 }
 
 config.key_tables = {
