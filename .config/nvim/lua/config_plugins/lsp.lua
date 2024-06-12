29822e5fd352d98b5f5891c860beb16d512f9e62
Pedro Videgain Barranco <pedro@vide.bar>
2024-06-12 11:38:23 +0200

    Added the matlab lsp server to nvim

--- .config/nvim/lua/config_plugins/lsp.lua
+++ .config/nvim/lua/config_plugins/lsp.lua
@@ -20,6 +20,17 @@ end
 lsp.pylsp.setup({ on_attach = default_on_attach })
 lsp.lua_ls.setup({ on_attach = default_on_attach })
 lsp.clangd.setup({ on_attach = default_on_attach })
+lsp.matlab_ls.setup({
+    on_attach = default_on_attach,
+    settings = {
+        MATLAB = {
+            -- The lsp server expects matlab to be installed here. The installation
+            -- directory can be obtained using the matlab command `matlabroot`.
+            installPath = vim.env.HOME .. "/.matlab",
+            telemetry = false,
+        },
+    },
+})
 lsp.texlab.setup({
     on_attach = default_on_attach,
     settings = {
